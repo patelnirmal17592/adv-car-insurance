bca7d24aec7747f21e68e4492f4487d7b0bb7b71
Nirmal Patel <patelnirmal17592@gmail.com>
2022-08-22 13:27:11 +1200

    search bar and button added

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,43 @@
+import style from "./SearchBar.module.css";
+import { useState } from "react";
+import { Box } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import LoadingButton from '@mui/lab/LoadingButton'
+
+import React from "react";
+
+const SearchBar = () => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <div className={style.searchBarContainer}>
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "40ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Ask me anything about car insurance..."
+          multiline
+          maxRows={4}
+          value={value}
+          onChange={handleChange}
+          className={style.userInput}
+        />
+        <LoadingButton loading={false} loadingIndicator="Loading…" variant="outlined" className={style.search} loadingPosition='center'>
+          Search
+        </LoadingButton>
+      </Box>
+    </div>
+  );
+};
+
+export default SearchBar;
---
+++ src/pages/carInsuranceHome.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchBar from '../components/SearchBar'
+
+const carInsuranceHome = () => {
+  return (
+    <>
+        <SearchBar></SearchBar>
+    </>
+  )
+}
+
+export default carInsuranceHome
