3cc3cadc0ea0997eaa838ab219fc4d4812956647
Nirmal Patel <patelnirmal17592@gmail.com>
2022-08-24 10:46:24 +1200

    got response from IBM watson

--- src/server/app.ts
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express();
-const dotenv = require('dotenv').config({ path: '../../env'})
-
-console.log(process.env);
-
-app.listen(3001, () => console.log('Port is running on 3001'));
